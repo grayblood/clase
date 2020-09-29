d632b3009cee8be90d88cae3bbc21c25396907dc
IES SABADELL <IES SABADELL@C3-26>
2020-09-29 17:02:50 +0200

    aaaaaa

---
+++ src/contigus.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class contigus {
+	static Scanner lector;
+	public static void main(String[] args) {
+		lector = new Scanner(System.in);
+		int a = lector.nextInt() ;
+		int b = lector.nextInt() ;
+		if () {
+			System.out.println("SI");
+		}
+		else {
+			System.out.println("NO");
+		}	
+	}
+}
+
